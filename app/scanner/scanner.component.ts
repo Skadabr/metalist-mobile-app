905861394d58582c9f2d3ece4d7d18cd1c1635d9
Sash730 <i_730@ukr.net>
2017-04-03 20:38:07 +0300

    add ofline

--- app/scanner/scanner.component.ts
+++ app/scanner/scanner.component.ts
@@ -19,7 +19,7 @@ export class ScannerComponent implements OnInit {
     isLoading: boolean;
     offline: boolean;
     switchFlash:boolean;
-    ticketStatusImage: string;
+    status: string;
     errorMessage: string;
     message: string;
     tribune: string;
@@ -28,6 +28,7 @@ export class ScannerComponent implements OnInit {
     tribunes: string[];
     tickets: any[];
     ticket: {};
+    sectorsInVip: string[];
 
     constructor(private router: Router,
                 private loginService: LoginService,
@@ -35,20 +36,27 @@ export class ScannerComponent implements OnInit {
                 private ticketService: TicketService,
                 private page: Page) {
         this.offline = false;
-        this.message = '';
         this.switchFlash = false;
         this.isLoading = false;
         this.role = '';
-        this.ticketStatusImage = '';
         this.count = 0;
         this.tribune = '';
         this.tribunes = ['east', 'west', 'north', 'vip'];
-        this.ticket = {};
+        this.sectorsInVip = ['VIP', 'VIP_BR', 'VIP_BL', 'VIP_AR', 'VIP_AL', 'SB_1', 'SB_7'];
     }
 
     ngOnInit() {
+        this.initTicketInfo();
         this.getUser();
+        this.loadTicketsForCheck();
+
         this.page.actionBarHidden = true;
+    }
+
+    initTicketInfo() {
+        this.message = '';
+        this.status = '';
+        this.ticket = {};
     }
 
     getUser() {
@@ -57,7 +65,6 @@ export class ScannerComponent implements OnInit {
             .subscribe(
                 user => {
                     this.role = user.role;
-                    console.log(this.role);
                 },
                 error =>  this.errorMessage = <any>error);
     }
@@ -68,7 +75,6 @@ export class ScannerComponent implements OnInit {
             .subscribe(
                 tickets => {
                     console.log('tickets count', tickets.length);
-                    applicationSettings.clear();
                     applicationSettings.setString("tickets", JSON.stringify(tickets));
                 },
                 error =>  this.errorMessage = <any>error
@@ -79,19 +85,24 @@ export class ScannerComponent implements OnInit {
         this.switchFlash = !this.switchFlash;
     }
 
+    toogleMode() {
+        this.offline = !this.offline;
+    }
+
     logoff() {
         this.loginService.logoff();
         this.router.navigate(["/login"]);
     }
 
     getCountTicketsByTribune(tribune) {
-        this.ticketService
-            .getCountTicketsByTribune(tribune)
-            .subscribe(
-                count => {
-                    this.count = count;
-                },
-                error =>  this.errorMessage = <any>error);
+        if (!this.offline) {
+            this.getCountTicketsOnline(tribune);
+        } else {
+            let tickets = applicationSettings.getString("tickets"),
+                defaultTickets = JSON.parse(tickets);
+
+            this.getCountTicketsOffline(defaultTickets, tribune);
+        }
     }
 
     displayActionDialog() {
@@ -102,7 +113,7 @@ export class ScannerComponent implements OnInit {
         };
         dialogs.action(options).then((result) => {
             this.tribune = result ? result : '';
-            console.log("result " + result);
+            console.log("result " , result, this.translate(result) );
             if (result) {
                 this.getCountTicketsByTribune( this.translate(result) );
             } else {
@@ -112,8 +123,15 @@ export class ScannerComponent implements OnInit {
     }
 
     getTranslateTribune(ticket) {
-       ticket.tribune = this.translateRu(ticket.tribune);
-       return  ticket;
+       return {
+           status: ticket.status,
+           headline: ticket.headline,
+           tribune: this.translateRu(ticket.tribune),
+           sector: ticket.sector,
+           row: ticket.row,
+           seat: ticket.seat,
+           code: ticket.code
+       };
     }
 
     changeLoadingStatus(status) {
@@ -129,6 +147,7 @@ export class ScannerComponent implements OnInit {
             this.message = 'Выберите трибуну.';
             return;
         }
+        this.initTicketInfo();
 
         this.barcodeScanner.scan({
             formats: "CODE_128",   // Pass in of you want to restrict scanning to certain types
@@ -138,6 +157,7 @@ export class ScannerComponent implements OnInit {
             resultDisplayDuration: 0   // Android only, default 1500 (ms), set to 0 to disable echoing the scanned text
         }).then( (result) => {
                 this.changeLoadingStatus(true);
+
                 if (!this.offline) {
                     this.checkTicketOnline(result.text);
                 } else {
@@ -155,22 +175,20 @@ export class ScannerComponent implements OnInit {
             .checkTicketStatus(this.translate(this.tribune), code)
             .subscribe(
                 data => {
-                    this.message = '';
                     if (!data.ticket) {
                         this.message = data.message;
                         this.count = data.count;
-                        this.ticketStatusImage = "res://notok";
+                        this.status = "notOk";
                     } else {
                         this.ticket = this.getTranslateTribune(data.ticket);
                         this.count = data.count;
 
                         if ( data.message ){
                             this.message = data.message;
-                            this.ticketStatusImage = "res://othertribune";
+                            this.status = "other";
                             return;
                         }
-                        this.ticketStatusImage = "res://ok";
-                        return;
+                        this.status = "ok";
                     }
                     this.changeLoadingStatus(false);
                 },
@@ -184,22 +202,69 @@ export class ScannerComponent implements OnInit {
     checkTicketOffline(code) {
         let tickets = applicationSettings.getString("tickets"),
             defaultTickets = JSON.parse(tickets),
-            ticketIndex = defaultTickets.findIndex(ticket => ticket.accessCode === code);
-
-        if ( ticketIndex < 0 ) {
+            [ ticket ] = defaultTickets.filter(ticket => (ticket.code === code && ticket.status === 'paid') );
+        //console.log('ticket123', ticket.tribune, ticket.status);
+        if ( !ticket ) {
             this.message = "Билет не действительный";
-            this.ticket = {};
-            this.ticketStatusImage = "res://notok";
+            this.status = "notOk";
         } else {
-            let ticket = defaultTickets[ticketIndex];
+            if (this.translate(this.tribune) === 'vip') {
+                ticket = this.checkTicketByVip(ticket);
+            } else {
+                ticket = this.checkTicketByTribune(ticket);
+            }
+            if (ticket.status === 'used') {
+                let updatedTickets = defaultTickets.filter(ticket => ticket.code !== code);
+                updatedTickets.push(ticket);
 
-            if (ticket.tribune !== this.tribune) {
-                this.message = "Другая трибуна";
-                this.ticket = ticket;
-                this.ticketStatusImage = "res://othertribune";
+                this.getCountTicketsOffline(updatedTickets, this.translate(this.tribune));
+                applicationSettings.setString("tickets", JSON.stringify(updatedTickets));
             }
         }
+    }
 
+    checkTicketByVip(ticket) {
+
+        if ( !this.sectorsInVip.filter(sector => sector === ticket.sector).length ) {
+            this.message = "Другая трибуна";
+            this.status = "other";
+        } else {
+            this.ticket = this.getTranslateTribune(ticket);
+            this.status = "ok";
+            ticket.status = 'used';
+        }
+        return ticket;
+    }
+
+    checkTicketByTribune(ticket) {
+        this.ticket = this.getTranslateTribune(ticket);
+        console.log('tribune123', ticket.tribune, this.translate(this.tribune));
+        if (ticket.tribune !== this.translate(this.tribune)) {
+            this.message = "Другая трибуна";
+            this.status = "other";
+        } else {
+            this.status = "ok";
+            ticket.status = 'used';
+        }
+        return ticket;
+    }
+
+    getCountTicketsOnline(tribune) {
+        this.ticketService
+            .getCountTicketsByTribune(tribune)
+            .subscribe(
+                count => {
+                    this.count = count;
+                },
+                error =>  this.errorMessage = <any>error);
+    }
+
+    getCountTicketsOffline(tickets, tribune) {
+        if ( tribune === 'vip' ) {
+            this.count = tickets.filter( ticket => (ticket.status === 'paid' &&  this.sectorsInVip.filter(sector => sector === ticket.sector).length) ).length;
+        } else {
+            this.count = tickets.filter( ticket => (ticket.status === 'paid' && ticket.tribune === tribune) ).length;
+        }
     }
 
     translate(direction) {
